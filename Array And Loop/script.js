5ff3f885ea5f76af6122a7b3f707e3f685209119
MUHAMMAD HUSSAIN SHAMIM <142653978+hussainshamim16@users.noreply.github.com>
2024-09-19 18:51:36 -0700

    Add files via upload

---
+++ Array And Loop/script.js
@@ -0,0 +1,81 @@
+console.log('hello world');
+
+
+// question no 1
+// var arr = [];
+// question no 1
+
+// question no 2
+//  var arr = [1,2,3,4,5]
+//  for(var i = 0; i<1;i++){
+//     console.log(arr)
+//  } 
+// question no 2
+
+// question no 3
+// for(var i = 1; i < 11 ; i++){
+//    console.log(i)
+// } 
+// question no 3
+
+
+// question no 4
+// var length = prompt("Enter the length of the table:");
+// var tableNumber = prompt("Enter the table number:");
+// for (var i = 1; i <= length; i++) {
+//     console.log(`${tableNumber} x ${i} = ${tableNumber * i}`);
+// }
+// question no 4
+
+// question no 5 & 7
+
+// var message = prompt('Enter Your food');
+// var food = ["cakes","applepie","cookies"];
+
+// for(var i = 0; i < 1 ; i++){
+//     if(message === 'cookie'){
+//         alert('Cookie is avaliable');
+//     }
+//     else if(message === 'pie'){
+//         alert('Pie is avaliable');
+//     }
+//     else if(message === 'cakes'){
+//         alert('Cakes is avaliable');
+//     }
+//     else{
+//         alert(message + " Is Not Avaliable");
+//     }
+// }
+
+// question no 5 & 7
+
+
+// question no 6
+// var num = [1,2,3,4,5,6,7,8,9,10]
+// for(var i = num.length-1; i >= 0 ; i--){
+//     console.log(num[i]);
+// } 
+// question no 6
+
+
+// question no 8 & 9
+// var arr = [1,4,6]
+// for(var i = 0; i < 1 ; i++){
+//    if(arr[0] > 6){
+//       console.log('😪');
+//    }
+//    else if(arr[1] > 6){
+//       console.log('😑');
+//    }
+//    else if(arr[2] >= 6){
+//       console.log('😁');
+//    }
+// }
+// question no 8 & 9
+
+
+// question no 10
+// for (var i = 1; i < 21; i++) {
+// console.log(`${5 * i}`);
+// }
+// question no 10
